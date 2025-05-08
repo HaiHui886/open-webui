95593dda7f958f6ba48ea114f7e448c23e984736
Sean <38311363@qq.com>
2025-05-08 19:47:40 +0800

    update

--- src/routes/(app)/workspace/models/edit/+page.svelte
+++ src/routes/(app)/workspace/models/edit/+page.svelte
@@ -23,10 +23,10 @@
 			});
 
 			if (!model) {
-				goto('/workspace/models');
+				goto('/endpoint/uivarpath/workspace/models');
 			}
 		} else {
-			goto('/workspace/models');
+			goto('/endpoint/uivarpath/workspace/models');
 		}
 	});
 
@@ -41,7 +41,7 @@
 				)
 			);
 			toast.success($i18n.t('Model updated successfully'));
-			await goto('/workspace/models');
+			await goto('/endpoint/uivarpath/workspace/models');
 		}
 	};
 </script>
